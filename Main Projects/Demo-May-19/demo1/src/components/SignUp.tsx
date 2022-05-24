e0e177f8151926a38c191c5481f4499f995a8edb
Jayaprakash-Aspirian <jayaprakash.palani@aspiresys.com>
2022-05-24 10:56:52 +0530

    24/05/2022

--- Main Projects/Demo-May-19/demo1/src/components/SignUp.tsx
+++ Main Projects/Demo-May-19/demo1/src/components/SignUp.tsx
@@ -1,38 +1,79 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link, Outlet, useNavigate } from 'react-router-dom'
 
+type State = {
+  firstname: string,
+  lastname: string,
+  email: any,
+  passwrord: any,
+  confirmpassword:any
+};
+
+
+const SubmitForm=()=>{
+  console.log("As1")
+  const [firstname,setFName] = useState("")
+  const [lastname,setLName] = useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword]= useState("") 
+  console.log("As1")
+  const[confirmpassword,setConfirmpassword]=useState("")
+  console.log("As2")
+  console.log(firstname)
+  console.log("As3")
+  let datas={"firstname":firstname,"lastname":lastname,"email":email,"password":password};
+  console.log("yes1")
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(datas)
+    
+  })
+  console.log("yes")
+  
+}
+
+
 const SignUp =() => {
+
+
+
   let navigate = useNavigate()
   return (
-   <div className='body-signup'>
+   <div className='body-signup' >
     <div className="container">
       <div className="row justify-content-center">
       <div className="col-md-5">
         <div className="card">
           <h2 className="card-title text-center" id="h2-signup">Register Page</h2>
             <div className="card-body py-md-4">
-              <form>
+              
                 <div className="form-group">
-                  <input type="text" className="form-control" id="name" placeholder="Enter the Name" />
+                  <input type="text" className="form-control" id="firstname" placeholder="Enter the First Name" required/>
                 </div>
                 <div className="form-group">
-                  <input type="email" className="form-control" id="email" placeholder="Enter the Email" />
+                  <input type="text" className="form-control" id="lastname" placeholder="Enter the Last Name" required/>
+                </div>
+                <div className="form-group">
+                  <input type="email" className="form-control" id="email" placeholder="Enter the Email" required/>
                 </div>           
                 <div className="form-group">
-                  <input type="password" className="form-control" id="password" placeholder="Enter the Password" />
+                  <input type="password" className="form-control" id="password" placeholder="Enter the Password" required/>
                 </div>
                 <div className="form-group">
-                  <input type="password" className="form-control" id="confirm-password" placeholder="Confirm-password" />
+                  <input type="password" className="form-control" id="confirmpassword" placeholder="Confirm-password" />
                 </div>
                 <div className="form-group">
                   <input type="text" className="form-control" id="mobile" placeholder="Enter the Mobile Number" />
                 </div> 
                 <div className="d-flex flex-row align-items-center justify-content-between">
                   <a className="btn btn-secondary" onClick={()=>{navigate("/login") }}>Login</a>
-                  <button className="btn btn-primary">Create Account</button>
+                  <button className="btn btn-primary" onClick={SubmitForm} >Create Account</button>
                 </div>
-                
-              </form>
+         
             </div>
           </div>
         </div>
